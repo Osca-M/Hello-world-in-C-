3204cafe1f7b202d2a19331cc7aead4768a1db76
Osca Mwongera <oscamwongera@gmail.com>
2022-04-19 16:20:55 +0300

    basic function

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+int sm(int a, int b) {
+    return a + b;
+}
+
+
 int main() {
 	std::cout << "Hello, World!" << std::endl;
+
+    int a = 8;
+    int b = 2;
+
+    std::cout << "The sum of " << a <<  " and " << b << " is "  << sm(a, b) <<std::endl;
+
     
     return 0;
 }
